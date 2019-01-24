ae1b4d6bb78c7ed2f64b2b1fad98521d5d41fe91
Thibaut <t.delplanque92@gmail.com>
2019-01-24 14:17:33 +0100

    add attributs dans ManagerJeu
    Ajout des attributs dans la classe ManagerJeu

--- src/com/apo/puissance4/ManagerJeu.java
+++ src/com/apo/puissance4/ManagerJeu.java
@@ -1,5 +1,44 @@
 package com.apo.puissance4;
 
 public class ManagerJeu {
+	
+	// **********************************************
+	// 					Attributs
+	// **********************************************
+	/**
+	 * nombre de joueur dans la partie, de base c'est deux
+	 * mais on peux imaginer plus ^^
+	 */
+	private int _nbJoueur;
+	/**
+	 * index du joeur jouant dans la liste de tout les joueurs
+	 */
+	private int _indexJoueurCourrant;
+	/**
+	 * la liste des joueurs jouant dans la partie
+	 */
+	private Joueur[] _joueurs;
+	/**
+	 * l'instance du joueur courrant
+	 */
+	private Joueur _joueurCourrant;
+	/**
+	 * l'instance de la grille de jeu
+	 */
+	private GrilleJeu _grille;
+	/**
+	 * le nombre de jeton à aligner dans la grille pour gagner
+	 */
+	private int _conditionVictoire;
+	/**
+	 * nombre de partie qui vont être jouer par les joueurs.
+	 */
+	private int _nbPartie;
+	/**
+	 * nombre de partie restante à jouer.
+	 */
+	private int _nbPartieRestante;
+	
+	
 
 }
---
+++ src/com/apo/puissance4/console/AffichageConsole.java
@@ -0,0 +1,10 @@
+package com.apo.puissance4.console;
+
+public class AffichageConsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
