3940850e3a408b8d539da8eaedc989d57960d0dc
Thibaut <t.delplanque92@gmail.com>
2019-01-26 13:58:41 +0100

    Commentarie de la classe Colonne

--- src/com/apo/puissance4/Colonne.java
+++ src/com/apo/puissance4/Colonne.java
@@ -4,8 +4,8 @@ import com.apo.puissance4.exception.CaseNotEmptyException;
 import com.apo.puissance4.exception.FullColumnException;
 
 /**	
- * La classe Colonne permet de g�rer une colonne dans un jeu de "puissance 4"
- * � l'aide de tableau @see Case.
+ * La classe Colonne permet de gérer une colonne dans un jeu de "puissance 4"
+ * à l'aide de tableau de Cases.
  * @author Thibaut Delplanque
  */
 public class Colonne {
@@ -14,8 +14,8 @@ public class Colonne {
 	// 					Attributs
 	// **********************************************
 	/**
-	 * tableau de @see Case qui repr�sente une colonne d'un jeu de puissance 4
-	 * le bas de la colonne se trouve � l'indice 0, et le haut � l'indice n 
+	 * tableau de Case qui représente une colonne d'un jeu de puissance 4
+	 * le bas de la colonne se trouve à l'indice 0, et le haut à l'indice n 
 	 */
 	private Case[] _colonne;
 	/**
@@ -31,7 +31,7 @@ public class Colonne {
 	// 				Getters Setters
 	// **********************************************
 	/**
-	 * Permet de r�cup�rer la taille de la colonne
+	 * Permet de récupérer la taille de la colonne
 	 * @return retourne un int pour la taille
 	 */
 	public int getTaille() {
@@ -39,26 +39,29 @@ public class Colonne {
 	}
 	
 	/**
-	 * Permet de r�cup�rer l'index de la case vide la plus en bas dans la colonne
+	 * Permet de récupérer l'index de la case vide la plus en bas dans la colonne
 	 * @return retourne un int pour l'index 
 	 */
 	public int getIndexCaseVide() {
 		return _indexCaseVide;
 	}
 	
-        /**
-         * Permet de retourner la case à l'index spécifié
-         * @param index : indexe de la case, de 0 à la taille de la colonne
-         * @return : la case à l'index spécifié
-         */
+    /**
+     * Permet de retourner la case à l'index spécifié
+     * @param index : indexe de la case, de 0 à la taille de la colonne
+     * @return : la case à l'index spécifié
+     */
 	public Case getCase(int index) {
 		if(index < 0 || index >= _tailleColonne)
 			throw new IndexOutOfBoundsException();
 		return _colonne[index];
 	}
 	
+	// **********************************************
+	// 				 Constructeurs
+	// **********************************************
 	/**
-	 * Initialise une instance de la classe @see Colonne avec une taille 
+	 * Initialise une instance de la classe Colonne avec une taille 
 	 * @param taille la taille de l'instance  
 	 */
 	public Colonne(int taille) {
@@ -70,11 +73,15 @@ public class Colonne {
 		}
 	}
 	
+	// **********************************************
+	// 					Méthodes
+	// **********************************************
+	
 	/**
-	 * Ajoute un @see Jeton dans la colonne, � la case la plus en bas 
-	 * @param jeton, le jeton � ajouter
-	 * @throws FullColumnException est renvoy� si la colunne est pleinne
-	 * @throws IllegalArgumentException est renvoy� si le jeton pass� en param�tre est null 
+	 * Ajoute un Jeton dans la colonne, à la case la plus en bas 
+	 * @param jeton, le jeton à ajouter
+	 * @throws FullColumnException est renvoyé si la colunne est pleinne
+	 * @throws IllegalArgumentException est renvoyé si le jeton passé en paramétre est null 
 	 */
 	public void ajouterJeton(Jeton jeton) throws IllegalArgumentException, FullColumnException {
 		if(_indexCaseVide >= _tailleColonne )
@@ -86,6 +93,10 @@ public class Colonne {
 			this.ajouterJeton(jeton);
 		}
 		_indexCaseVide++;
+	}
+	
+	public boolean isFull() {
+		return _indexCaseVide >= _tailleColonne;
 	}
 	
 	
