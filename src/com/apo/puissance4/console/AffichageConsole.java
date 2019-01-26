718552ef0712515e9739940859d0b6ae2dda1cf4
Thibaut <t.delplanque92@gmail.com>
2019-01-26 12:22:07 +0100

    scripte pour une partie classique dans la console
    problème avec la vérif de fin de partie

--- src/com/apo/puissance4/console/AffichageConsole.java
+++ src/com/apo/puissance4/console/AffichageConsole.java
@@ -2,7 +2,10 @@ package com.apo.puissance4.console;
 
 import java.util.Scanner;
 
+import com.apo.puissance4.GrilleJeu;
 import com.apo.puissance4.ManagerJeu;
+import com.apo.puissance4.exception.FullColumnException;
+import com.apo.puissance4.exception.KeyDoNotExistException;
 
 public class AffichageConsole {
 
@@ -54,16 +57,70 @@ public class AffichageConsole {
 
 	private static void partieLocal() {
 		jeu = new ManagerJeu();
-		System.out.print("Saisissez le nombre de joueur : ");
-		String saisie = sc.nextLine();
-		try {
-			jeu.set_nbJoueur(Integer.parseInt(saisie));
-		} catch (Exception e) {
-			quitter();
-		}
+		boolean choisie;
+		System.out.println("Partie rapide ou personnalisé ?");
+		do {
+			choisie = true;
+			System.out.println("1    : rapide");
+			System.out.println("2    : personnalisé");
+			System.out.println("exit : quitter");
+			String saisie = sc.nextLine();
+			
+			switch (saisie) {
+			case "1":
+				break;
+			case "2":
+				personnalise();
+				break;
+			case "exit":
+				quitter();
+				break;
+			default:
+				choisie = false;
+				System.out.println("saisie incorrecte");
+				break;
+			}
+		}while(!choisie);
+		jouer();
 		
+	}
+	
+	private static void personnalise() {
+		
+	}
+	
+	private static void jouer() {
+		boolean tourFinie;
+		do {
+			do {
+				tourFinie = true;
+				System.out.print("Partie n°" + jeu.getNumPartie() + "/" + jeu.getNbPartie() + " - ");
+				System.out.println("Tour de " + jeu.getNomJoueurCourrant());
+				System.out.println();
+				System.out.println(jeu.affichage());
+				System.out.print("Entrée le  nom de la case: ");
+				String saisie = sc.nextLine();
+				
+				try {
+					jeu.jouer(saisie);
+				} catch (FullColumnException e) {
+					// TODO Auto-generated catch block
+					System.out.println("la colonne est pleinne ! Gros Beta !! ");
+					tourFinie = false;
+				} catch (KeyDoNotExistException e) {
+					// TODO Auto-generated catch block
+					System.out.println("appuie sur une touche qui existe. Patate! -_- ");
+					tourFinie = false;
+				}catch(IllegalArgumentException e) {
+					System.out.println("Une erreur c'est produite");
+					quitter();
+				}
+			}while(!tourFinie);
+			
+		}while(!jeu.partieFinie());
 		
 		
 	}
-
+	
 }
+
